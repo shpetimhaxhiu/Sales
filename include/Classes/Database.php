afa83af7bac588b3483bac74af05043469c4c78d
shpetimhaxhiu <shpetim.h@gmail.com>
2018-04-11 18:06:44 +0100

    [Change] - ndryshimi i sql

--- include/Classes/Database.php
+++ include/Classes/Database.php
@@ -7,7 +7,7 @@
 	{
 		public function lidhja()
 		{
-      $db = mysqli_connect('localhost','root','njeriop', 'test2');
+      $db = mysqli_connect('localhost','root','njeriop123!@#', 'test2');
 			if (mysqli_connect_errno($db)) {
 			    return mysqli_connect_error();
 			} else {
